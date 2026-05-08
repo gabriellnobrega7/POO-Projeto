21aa9a23579c1df74298c9967df17b71be96f2d6
Gabriel Soares <gabriellnobrega21@gmail.com>
2026-05-08 22:52:55 +0000

    all

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -5,22 +5,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.UserService;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 @RequestMapping("/api/v1/users")
 
-public class UserController {
-    
-    UserService userservice;
+public class UserController<UserModel> {
+    @Autowired //cria os contrutores automatico
+    UserService userService;
 
     @GetMapping("/allUsers")
     public List<UserModel> getAllUsers(){
-        return userservice.getAll();
+        return userService.getAllUsers();
 
     }
+
+    @PostMapping("/add")
+    public UserModel newUser(@RequestBody UserModel user){
+
+        return userService.addNewUser(user);
+    }
+    
+
+
 
     @GetMapping("/hello")
     public String hello() {
--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,8 +1,33 @@
 package com.example.demo.service;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.UserModel;
+import com.example.demo.repository.UserRepository;
 
 @Service
 public class UserService {
     
+    public List<UserModel> getAllUsers(){
+
+        @Autowired
+        UserRepository userRepository;
+
+        public List<UserModel> getAllUsers(){
+
+            return userRepository.findAll();
+
+        }
+
+        public UserModel addNewUser(UserModel user){
+
+            return userRepository.save(user);
+        }
+    }
+
+
 }
